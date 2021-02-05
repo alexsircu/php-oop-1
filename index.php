9ed4b9cd68b10db90b632fc830b1f94441a28525
Alex Sircu <alex.sricu@gmail.com>
2021-02-05 16:47:47 +0100

    Struttura base

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+  class Book {
+
+    public $title;
+    public $author;
+    public $price;
+    public $genre;
+
+  }
+
+  $firstBook = new Book();
+  var_dump($firstBook);
+
+  $secondBook = new Book();
+  var_dump($secondBook);
+
+  $thirdBook = new Book();
+  var_dump($thirdBook);
