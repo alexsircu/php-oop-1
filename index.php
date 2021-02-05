e6fc906df4a439720632d0c8ba46ce9d26660b64
Alex Sircu <alex.sricu@gmail.com>
2021-02-05 17:23:40 +0100

    Test var_dump ed echo con tre classi book

--- index.php
+++ index.php
@@ -9,10 +9,24 @@
   }
 
   $firstBook = new Book();
+  $firstBook->title = "Le vostre zone erronee";
+  $firstBook->author = "Wayne W. Dyer";
+  $firstBook->price = 10.00;
+  $firstBook->genre = "Psicologico";
   var_dump($firstBook);
 
   $secondBook = new Book();
+  $secondBook->title = "Pensieri lenti e veloci";
+  $secondBook->author = "Daniel Kahneman";
+  $secondBook->price = 17.00;
+  $secondBook->genre = "Psicologico";
   var_dump($secondBook);
 
   $thirdBook = new Book();
+  $thirdBook->title = "Il fu Mattia Pascal";
+  $thirdBook->author = "Luigi Pirandello";
+  $thirdBook->price = 6.00;
+  $thirdBook->genre = "Romanzo";
   var_dump($thirdBook);
+
+  echo $thirdBook->title;
